41d339248e09c1b3051f4f93fd65ae82202c8a3d
prasan.samtani <prasan.samtani@hulu.com>
2014-04-04 08:11:37 -0700

    Added enemy

--- spark2014-fall-prasan/game.py
+++ spark2014-fall-prasan/game.py
@@ -16,22 +16,60 @@ kevin = pygame.image.load("kobe.jpg")
 kobejumping = pygame.image.load("kobejumping.jpg")
 zombie = pygame.image.load("zombie.gif")
 gameover = pygame.image.load("gameOver.jpg")
-
+hoop=pygame.image.load("hoop.jpg")
+durant=pygame.image.load("durant.jpg")
+lebron=pygame.image.load ("lebron.jpg")
 alive = True
 jumping = False
 backDown = False
 x = 0
 y = 5
+score=0
+lebronx= 4
+lebrony= 5
 
+durantx= 6
+duranty= 5
 while (alive == True):
     screen.blit(background, (0,0))
-
+    screen.blit (hoop, convert_to_screen_space(7,3))
     if jumping == True:
         screen.blit(kobejumping, convert_to_screen_space(x,y))
     else:
         screen.blit(kevin, convert_to_screen_space(x,y))
-    
-    
+
+    screen.blit(durant,convert_to_screen_space(durantx,duranty))
+    screen.blit(lebron,convert_to_screen_space(lebronx,lebrony))
+    if (x==7) and (y==3) :
+       score= score+1
+       x=0
+       y=5
+       jumping = False
+       backDown = False
+
+    if ((x==durantx) and (y==duranty)) or ((x==lebronx) and (y==lebrony)):
+       x=0
+       y=5
+       jumping = False
+       backDown = False
+       score = score - 1
+
+    duranty=duranty-0.5
+    if duranty== 0:
+       duranty=5
+  
+    b = pygame.Surface((100, 100))
+    b = b.convert()
+    b.fill((200, 200, 200))
+           
+    # Display some text
+    font = pygame.font.Font(None, 36)
+    text = font.render(str(score), 1, (10, 10, 10))
+    textpos = text.get_rect()
+    textpos.centerx = 50
+    b.blit(text, textpos)
+    screen.blit(b, (0, 0))
+
     pygame.display.flip()
     time.sleep(0.1)
 
